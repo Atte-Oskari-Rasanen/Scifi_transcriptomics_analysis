0c169caeba07cf6ae8711ec9227d542cdf38fdd6
Atte-Oskari-Rasanen <70641619+Atte-Oskari-Rasanen@users.noreply.github.com>
2022-03-22 15:38:25 +0100

    Update match_starsolo.py
    There was a faulty indentation

--- match_starsolo.py
+++ match_starsolo.py
@@ -27,9 +27,32 @@ import random
 from functools import reduce
 import csv 
 import tempfile
+np1 = np.arange(1,10)
+np1
+np2 = np.array([[1,4,3], [4,10,11], [1,3,2,15,6]], dtype=object)
+np2
 
 
+outer_product = np.outer(np1, np2)
+outer_product
 '''
+Vectorisation approach - take the seq ids as a np 1 and the clusters with seq ids as np 2. 
+np1 = 1d np array
+np2 = 2d np array
+
+#each element of this np array corresponds to the seq id when counting from 0 to N
+[[1,4,3], [4,10,11] [1,3,2,15,6]]
+
+We go over each of them and multiply by the element of np1 and then 
+
+seq id: 4
+
+So products when each element of np2 are calculated are:
+4
+16
+12
+
+
 Pool and map() arent suited to situations that need to maintain state over time or, especially, situations where there are 
 two or more different operations that need to run and interact with each other in some way. For these kinds of problems, one
 needs to make use of somewhat more complex features of the multiprocessing module, such as Process, Queue and Event. Using 
@@ -47,9 +70,10 @@ active at any given moment.
 basePath = 'SoloNova10x_correctAAV/'
 basePath= "/media/data/AtteR/scifi-analysis/scifi6/2nd_try/output_enriched/"
 
-
 r21_files = sorted(glob.glob(basePath+'*R21.fastq.gz'))
-r21_files= [item for item in r21_files if 'oDT' not in item]
+r21_files= [item for item in r21_files if 'oDT' not in item]  
+r21_files= [item for item in r21_files if 'SciFi_Pool1-2' in item]  
+
 r21_files
 r21_sc_files = {}
 
@@ -102,7 +126,7 @@ for r21, sc_f in r21_sc_files.items():
 
 
     nprocs = mp.cpu_count()
-    p_size = nprocs - 25
+    p_size = nprocs - 5
     #############
 
     def progressbar(it, prefix="", size=100, file=sys.stdout):
@@ -159,66 +183,68 @@ for r21, sc_f in r21_sc_files.items():
         return(final_df)
     #############
 
-    def reader(i,q):
-        message = q.get() #retrieves the queue whereas the writer function puts its output into the queue
-        print(message)
-    def slice_data(data, nprocs):
-        aver, res = divmod(len(data), nprocs)
-        nums = []
-        for proc in range(nprocs):
-            if proc < res:
-                nums.append(aver + 1)
+def reader(i,q):
+    message = q.get() #retrieves the queue whereas the writer function puts its output into the queue
+    print(message)
+def slice_data(data, nprocs):
+    aver, res = divmod(len(data), nprocs)
+    nums = []
+    for proc in range(nprocs):
+        if proc < res:
+            nums.append(aver + 1)
+        else:
+            nums.append(aver)
+    count = 0
+    slices = []
+    for proc in range(nprocs):
+        slices.append(data[count: count+nums[proc]])
+        count += nums[proc]
+    return slices
+
+#I can either run this script on the file prior to demultiplexing. This means that when I demultiplex the files, I must index
+#the lines... So instead run this script on the demultiplexed samples (so first starcode, then this), then map and then import into anndata
+
+#I must iterate over the files and get the R21 read and then the corresponding starcode tsv
+
+#multiprocess the files
+p = Pool(p_size)
+m = Manager()
+
+q = m.Queue()
+seq_lists = slice_data(range(len(seqs)), nprocs)
+print(seq_lists)
+#multi_result = [pool.apply_async(power_n_list, (inp, 2)) for inp in inp_lists] uniq_bcs_cell
+multi_result = [p.apply_async(unique_bcs_per_seq, (BC_clusters_dict, seq,r21,q,)) for seq in seq_lists]
+final_result = [result.get() for result in multi_result]
+'''
+#final_result
+vect_func = np.vectorize(unique_bcs_per_seq)
+for seq in seq_lists
+    vect_func(BC_clusters_dict, seq)
+'''
+#get the files from the tmp dir
+count_files = [i for i in glob.glob(tmp_dir +"/*.csv")]
+def common_name(sa, sb):
+    """ returns the longest common substring from the beginning of sa and sb """
+    def _iter():
+        for a, b in zip(sa, sb):
+            if a == b:
+                yield a
             else:
-                nums.append(aver)
-        count = 0
-        slices = []
-        for proc in range(nprocs):
-            slices.append(data[count: count+nums[proc]])
-            count += nums[proc]
-        return slices
+                return
 
-    #I can either run this script on the file prior to demultiplexing. This means that when I demultiplex the files, I must index
-    #the lines... So instead run this script on the demultiplexed samples (so first starcode, then this), then map and then import into anndata
+    return ''.join(_iter())
+shared_path = common_name(count_files[0], count_files[1])
 
-    #I must iterate over the files and get the R21 read and then the corresponding starcode tsv
-
-    #multiprocess the files
-    p = Pool(p_size)
-    m = Manager()
-
-    q = m.Queue()
-    seq_lists = slice_data(range(len(seqs)), nprocs)
-    print(seq_lists)
-    #multi_result = [pool.apply_async(power_n_list, (inp, 2)) for inp in inp_lists] uniq_bcs_cell
-    multi_result = [p.apply_async(unique_bcs_per_seq, (BC_clusters_dict, seq,r21,q,)) for seq in seq_lists]
-    final_result = [result.get() for result in multi_result]
-    #final_result
-
-
-
-    #get the files from the tmp dir
-    count_files = [i for i in glob.glob(tmp_dir +"/*.csv")]
-    def common_name(sa, sb):
-        """ returns the longest common substring from the beginning of sa and sb """
-        def _iter():
-            for a, b in zip(sa, sb):
-                if a == b:
-                    yield a
-                else:
-                    return
-
-        return ''.join(_iter())
-    shared_path = common_name(count_files[0], count_files[1])
-
-    shared_name = '_'.join(shared_path.split("/")[-1].split("_")[:-3])
-    #combine all files in the list
-    combined_csv = pd.concat([pd.read_csv(f) for f in count_files ])
-    #export to csv
-    combined_csv.to_csv(basePath + "/" + shared_name + "_Uniq_BCs_per_cell.csv", index=False)
-    df_counts_full = pd.read_csv(basePath + "/" + shared_name + "_Uniq_BCs_per_cell.csv")
-    df_counts_full =df_counts_full.sort_values(by = "Cell", ascending=True)
-    del df_counts_full["Unnamed: 0"]
-    #df_counts_full
-    df_counts_full.to_csv(basePath + "/" + shared_name + "_Uniq_BCs_per_cell.csv", index=False)
-    print("Done")
-    tmp_loc.cleanup()
+shared_name = '_'.join(shared_path.split("/")[-1].split("_")[:-3])
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in count_files ])
+#export to csv
+combined_csv.to_csv(basePath + "/" + shared_name + "_Uniq_BCs_per_cell.csv", index=False)
+df_counts_full = pd.read_csv(basePath + "/" + shared_name + "_Uniq_BCs_per_cell.csv")
+df_counts_full =df_counts_full.sort_values(by = "Cell", ascending=True)
+del df_counts_full["Unnamed: 0"]
+#df_counts_full
+df_counts_full.to_csv(basePath + "/" + shared_name + "_Uniq_BCs_per_cell.csv", index=False)
+print("Done")
+tmp_loc.cleanup()
