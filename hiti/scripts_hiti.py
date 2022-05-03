51f427bb39657a3f077b1d9864282f73505ad275
Atte-Oskari-Rasanen <70641619+Atte-Oskari-Rasanen@users.noreply.github.com>
2022-05-03 14:57:56 +0200

    Update scripts_hiti.py
    added classes and functions for nt alignment

--- hiti/scripts_hiti.py
+++ hiti/scripts_hiti.py
@@ -186,6 +186,7 @@ def create_datadict(base_path):
 
     return(data_dict)
 
+
 def save_fasta(filename, full_df, target_sequence):
     id_f = 1
     ref="mcherry_p3_seq_ref"
@@ -202,150 +203,129 @@ def save_fasta(filename, full_df, target_sequence):
             id_f+=1
     print("Saved!")
 
-class aa_alignments():
+import re
+################
+#ALIGNMENTS
+from Bio.SubsMat import MatrixInfo as matlist
+Bio.Align.substitution_matrices
 
-    def __init__(self, NT_seq_fasta, corr_frame, alt_frame,out_html):
-        self.NT_seq_fasta=NT_seq_fasta
-        self.out_html=out_html
-        self.corr_frame=corr_frame
-        self.alt_frame=alt_frame
+class align_local():
+    aligned_data=dict()
+    def __init__(self, amplicon, target_sequence,gop=-3, gep=-1):
+        self.amplicon=amplicon
+        self.target_sequence=target_sequence
+        self.gop=gop
+        self.gep=gep
 
-    def find_colour(aa,color_scheme):
-        col=[key for key,val in color_scheme.items() if any(aa in s for s in val)]
-        out="".join(col)
-        if out==None or not col:
-            out="BLACK"
-            return(out)
+    def align(self):
+        alignments = pairwise2.align.localxx(self.target_sequence, self.amplicon)
+
+        #alignments = pairwise2.align.globalms(target_sequence, seq_and_perc[group][:,0],  2, -1, -.5, -.1)
+        alignm=format_alignment(*alignments[0])
+        #make string objects, save into a list. then count which one has least ----, cut off the rest of the reference based on this?
+        seq_align = alignm.split("\n")[2]
+        return(seq_align)
+
+class align_local2():
+    aligned_data=dict()
+    def __init__(self, amplicon, target_sequence,gop=-3, gep=-1):
+        self.amplicon=amplicon
+        self.target_sequence=target_sequence
+        self.gop=gop
+        self.gep=gep
+
+    def align(self):
+        alignment, score, start_end_positions = local_pairwise_align_ssw(DNA(self.target_sequence),DNA(self.amplicon),gap_open_penalty =self.gop,gap_extend_penalty = self.gep)
+        out_align = ('-'*start_end_positions[0][0])+str(alignment[1])+('-'*(len(target_sequence)-start_end_positions[0][1]-1))
+        return(out_align)
+
+class align_local3():
+    aligned_data=dict()
+    def __init__(self, amplicon, target_sequence,gop=-3, gep=-1):
+        self.amplicon=amplicon
+        self.target_sequence=target_sequence
+        self.gop=gop
+        self.gep=gep
+    def align(self):
+        alignments = pairwise2.align.localms(self.target_sequence, self.amplicon, 2, -1, self.gop, self.gep)
+        #alignments = pairwise2.align.localms(self.target_sequence, self.amplicon, 2, -1, -.5, -.1)
+        #alignments= pairwise2.align.localds(self.target_sequence, self.amplicon, 2, -1, -.5, -.1)
+        alignm=format_alignment(*alignments[0])
+        seq_align = alignm.split("\n")[2]
+        return(seq_align)
+class align_global():
+    aligned_data=dict()
+
+    def __init__(self, amplicon, target_sequence,gop=-3, gep=-1):
+        self.amplicon=amplicon
+        self.target_sequence=target_sequence
+        self.gop=gop
+        self.gep=gep
+    def align(self):
+        alignments = pairwise2.align.globalms(self.target_sequence, self.amplicon,  2, -1, -.5, -.1)
+        alignm=format_alignment(*alignments[0])
+        seq_align = alignm.split("\n")[2]
+        return(seq_align)
+    #nt_count=count_nts(seq_align)
+    #seq_and_perc[group]["match"]
+class align_global2():
+    aligned_data=dict()
+
+    def __init__(self, amplicon, target_sequence,gop=-3, gep=-1):
+        self.amplicon=amplicon
+        self.target_sequence=target_sequence
+        self.gop=gop
+        self.gep=gep
+    def align(self):
+        alignments = pairwise2.align.globalxx(self.target_sequence, self.amplicon)
+        alignm=format_alignment(*alignments[0])
+        #make string objects, save into a list. then count which one has least ----, cut off the rest of the reference based on this?
+        seq_align = alignm.split("\n")[2]
+        return(seq_align)
+
+import inspect
+
+def align_trimmer(aligned_data):
+    for id in aligned_data.keys():
+        if len(aligned_data[id])==len(target_sequence):
+            continue
+        if len(aligned_data[id])>len(target_sequence):
+            N_dashes=len(target_sequence)-len(aligned_data[id])
+            aligned_data[id]=aligned_data[id][:N_dashes]
+            print("Seq length larger than ref by " + str(N_dashes) + " ... \n After removal length: " + str(len(aligned_data[id][:N_dashes])))
         else:
-            return(out)
+            N_dashes=len(target_sequence)-len(aligned_data[id])
+            aligned_data[id]=aligned_data[id]+ N_dashes*"-"
+            print("Seq length smaller than ref by " + str(N_dashes) + " ... \n After addition length: " + str(len(aligned_data[id])))
+    return(aligned_data)
+def write_align(aligned_data, filename):
+    with open(filename, "w") as handle:
+        header=">0"+" Ref_seq"
+        handle.write(header + "\n" + target_sequence + "\n")
+        for seq_i in aligned_data.keys():
+            handle.write(seq_i + "\n" + aligned_data[seq_i] + "\n")
 
-    def align_ampl_x_ref(self):
-        refs_aa_frames={}
-        aa_and_perc={}
-        out_of_frames=[0,1,2]
-        out_of_frames.remove(self.corr_frame)
-
-        #Translate the seq into AAs
-        for record in SeqIO.parse(self.NT_seq_fasta, "fasta"):
-            if record.id=="0":
-                refs_aa_frames["Frame:" + str(self.corr_frame)]=str(Seq(record.seq[self.corr_frame:]).translate())
-                for alt_frame in out_of_frames:
-                    refs_aa_frames["Frame:" + str(self.alt_frame)]=str(Seq(record.seq[self.alt_frame:]).translate())
-            else:
-                aa_and_perc[">"+str(record.description) + "_transl.frame:" + str(corr_frame)]=str(Seq(record.seq[corr_frame:]).translate())
-        #you go over the ref seqs in different frames and align all the amplicons to them. save the alignment into the df's specific column. initially
-        #save into a list or such
-        ref_x_alignment={}
-        for frame_ref in refs_aa_frames.keys():
-            alignments_per_ref=[]
-            for ampl in aa_and_perc.keys():
-                matches=SequenceMatcher(None,refs_aa_frames[frame_ref],aa_and_perc[ampl])
-                seqs=[]
-                #you use range_line so that when you fill the remnants from left side of the match, you wont keep adding from
-                #beginning since in the end, we merge the seq list elements into the whole alignment of the amplicon against the ref
-                range_line=0
-                for i in range(len(matches.get_matching_blocks())):
-                    match=matches.get_matching_blocks()[i]
-                    seqs.append(len(refs_aa_frames[frame_ref][range_line:match.a])*"-"+str(aa_and_perc[ampl])[match.b:match.b+match.size])
-                    range_line=match.a+match.size
-                alignments_per_ref.append(''.join(seqs))
-            ref_x_alignment[frame_ref + "|Ref:" +refs_aa_frames[frame_ref]]=alignments_per_ref
-
-        seq_info=["Seq_info"]+list(aa_and_perc.keys())
-        ref_x_alig_list=[]
-        for keys, values in ref_x_alignment.items():
-            print("".join(list((keys))[:]))
-            #ref_x_alig_list.append([("".join(list((keys))))]+list(values))
-            ref_x_alig_list.append([keys]+list(values))
-
-        #df = pd.DataFrame([seq_info, ref_x_alig_list[0],ref_x_alig_list[1],ref_x_alig_list[2]], columns =[seq_info[0],ref_x_alig_list[0][0], ref_x_alig_list[1][0], ref_x_alig_list[2][0]])
-        df = pd.DataFrame(data= {seq_info[0]: seq_info[1:], ref_x_alig_list[0][0]:ref_x_alig_list[0][1:], ref_x_alig_list[1][0]:ref_x_alig_list[1][1:], ref_x_alig_list[2][0]:ref_x_alig_list[2][1:]})
-
-        seqinfos=[]
-        aligs_merged_seq=[]
-        aligs_merged_ref=[]
-        frame_info=[]
-        for ampl_row in range(round(len(df.index)*0.1)):
-            match1 = SequenceMatcher(None, df.columns[1].split(":")[-1], df.iloc[ampl_row,1]).find_longest_match(0, len(df.columns[1].split(":")[-1]), 0, len(df.iloc[ampl_row,1]))
-            end_of_match=match1.b+match1.size
-            matched_ampl1= len(df.columns[1].split(":")[-1][:match1.a])*"-" + str(df.iloc[ampl_row,1][match1.b:]) + "|"
-            seq_inframe1=matched_ampl1[0:end_of_match] + "|"
-            ref_inframe1=df.columns[1].split(":")[-1][0:end_of_match] + "|"
-            #get the matches with the other reading frames (i.e. the last 2 cols of the df)
-            match2_a = SequenceMatcher(None, df.columns[2].split(":")[-1], df.iloc[ampl_row,2]).find_longest_match(0, len(df.columns[2].split(":")[-1]), 0, len(df.iloc[ampl_row,2]))
-            match2_b = SequenceMatcher(None, df.columns[3].split(":")[-1], df.iloc[ampl_row,3]).find_longest_match(0, len(df.columns[3].split(":")[-1]), 0, len(df.iloc[ampl_row,3]))
-            if match2_a.size>match2_b.size:
-                matched_ampl2= len(df.columns[2].split(":")[-1][:match2_a.a])*"-" + str(df.iloc[ampl_row,2][match2_a.b:]) + "|"
-                ref_outframe2=df.columns[2].split(":")[-1][0:end_of_match] + "|"
-                # seq_inframe2=matched_ampl2[0:end_of_match]
-                seq_outframe=matched_ampl2[end_of_match:]
-            else:
-                matched_ampl2= len(df.columns[3].split(":")[-1][:match2_b.a])*"-" + str(df.iloc[ampl_row,3][match2_b.b:]) + "|"
-                ref_outframe2=df.columns[3].split(":")[-1][end_of_match:] + "|"
-                seq_outframe=matched_ampl2[end_of_match:]
-                # seq_inframe2=matched_ampl2[0:end_of_match]
-                seq_outframe=matched_ampl2[end_of_match:]
-                frame_info.append("Frame:+1 " + ref_inframe1.index("|")*" " + "Frame:+2")
-            merged_align_seq=seq_inframe1+seq_outframe
-
-            merged_align_ref=ref_inframe1+ref_outframe2
-
-            seqinfos.append(df.iloc[ampl_row,0])
-            #aligs_merged_seq.append(merged_align_seq[0:round(len(aligs_merged_seq)*0.6)])
-            aligs_merged_seq.append(merged_align_seq)
-            #aligs_merged_ref.append(merged_align_ref[0:round(len(merged_align_ref)*0.6)])
-            aligs_merged_ref.append(merged_align_ref)
-
-
-            #using lesk color code scheme:
-            #color_scheme={"RED": [["#FF0000"], ["D", "E"]], "BLUE":[["#6495ED"],["K", "R"]], "GREEN":[["#9FE2BF"], ["C", "V", "I", "L", "P", "F", "Y", "M", "W"]], "ORANGE":[["#FF7F50"],["G", "A", "S", "T"]], "MAGENTA":[["#DE3163"], ["N", "Q", "H"]], "BLACK": [["#000000"],["-","|", "*"]]}
-            color_scheme={"RED": [["#FF0000"], ["D", "E"]], "BLUE":[["#6495ED"],["K", "R"]], "GREEN":[["#9FE2BF"], ["C", "V", "I", "L", "P", "F", "Y", "M", "W"]], "ORANGE":[["#FF7F50"],["G", "A", "S", "T"]], "MAGENTA":[["#DE3163"], ["N", "Q", "H"]], "BLACK": [["#000000"]]}
-            def find_colour(aa,color_scheme):
-                col=[key for key,val in color_scheme.items() if any(aa in s for s in val)]
-                out="".join(col)
-                if out==None or not col:
-                    out="BLACK"
-                    return(out)
-                else:
-                    return(out)
-
-
-            f = open(self.out_html,"w")
-            for info, a1,a2, frame in zip(seqinfos, aligs_merged_seq, aligs_merged_ref,frame_info):
-                aa1_list=[]
-                aa2_list=[]
-
-                print(frame)
-                a1,a2=a1[0:round(len(a1)*0.6)], a2[0:round(len(a2)*0.6)]
-                #f.write("<html> \n <body> <p>"+ "_".join(info.split("_")[:1]) +"</p> <p>")
-                f.write('<!DOCTYPE html><html> <head><link rel="stylesheet" href="format.css"></head> <meta charset="utf-8"> <body> <p>'+ "_".join(info.split("_")[:2]) + "<br>" + frame +'</p> <p>')
-
-            #go over the individual AAs of the alignemnts
-                #write the html colouring into a string var for each aa and append into a list, then join and make a massive string,
-                # then after all AAs have been iterated over for the certain seq, then write into the html 
-                for aa1,aa2, in zip(a1,a2):
-                    try:
-                        aa2_list.append('<span style="color:'+ color_scheme[find_colour(aa2,color_scheme)][0][0]+ '">' + aa2 + '</span>')
-                    except KeyError:
-                        print("Keyerror with aa2 being: " + aa2)
-                    try:
-                        aa1_list.append('<span style="color:'+ color_scheme[find_colour(aa1,color_scheme)][0][0]+ '">' + aa1 + '</span>')
-                    except KeyError:
-                        print("Keyerror with aa1 being: " + aa1)
-                    
-                    # aa2_list.append('<span style="color:'+ color_scheme[find_colour(aa2,color_scheme)][0][0]+ '">' + aa2 + '</span>')
-                    # aa1_list.append('<span style="color:'+ color_scheme[find_colour(aa1,color_scheme)][0][0]+ '">' + aa1 + '</span>')
-
-                    #print("============")
-                coloured_ref="".join(aa2_list)
-
-                coloured_seq="".join(aa1_list)
-
-                f.write(coloured_ref +"<br>")
-                f.write(coloured_seq)
-                f.write("</p>")
-
-            f.write("</body></html>")
-            f.close()
-        print("alignment file saved into " + "/".join(self.out_html.split("/")[:-1]) + " as " + self.out_html.split("/")[-1])
+#takes in the df and the choice of the alignment method. methods are found in class
+#the class must be instantiated inside the function and the appropriate method is called
+#by index passed by the user into function
+def aligner(full_df, target_sequence, align_method, filename, gop=3, gep=1):
+    align_class = {"align_local": align_local,
+            "align_local2": align_local2,
+            "align_local3":align_local3,
+            "align_global":align_global,
+            "align_global2":align_global2}  
+    id_f=1
+    aligner_init = align_class.get(str(align_method), None)  # Get the chosen class, or None if input is bad
+    aligner_init
+    aligned_data=dict()
+    #align all the data, save into dict, then ensure that all the seqs are same length (take the longest seq). IF not, then add padding!
+    for seq_i in range(len(full_df.iloc[:,-1])):
+        #yield iteratively the header of the certain seq and the corresponding seq
+        header=">"+ str(id_f)+"CluSeq:" + str((round(full_df.iloc[seq_i,-3],5))) + "_sd:" + str((round(full_df.iloc[seq_i,-1],5)))
+        seq_obj_align = aligner_init(full_df.iloc[seq_i,0], target_sequence, gop, gep).align()
+        seq_obj_align = re.sub(r'[(\d|\s]', '', seq_obj_align) #remove digits from the string caused by the alignment and empty spaces from the start
+        aligned_data[header]=seq_obj_align
+        id_f+=1
+    aligned_data_trim=align_trimmer(aligned_data)
+    write_align(aligned_data_trim, filename)
 
