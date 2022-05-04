f6c3186221834fb8c65890e52107e77776ee71d0
Atte-Oskari-Rasanen <70641619+Atte-Oskari-Rasanen@users.noreply.github.com>
2022-05-04 14:47:49 +0200

    Update scripts_hiti.py
    added the AA alignment scripts and made into organised functions

--- hiti/scripts_hiti.py
+++ hiti/scripts_hiti.py
@@ -35,6 +35,21 @@ from alignment_scripts import *
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import statistics as st
+def save_fasta(filename, full_df, target_sequence):
+    id_f = 1
+    ref="Ref"
+    with open(filename, "w") as handle:
+        seq_obj = SeqRecord(Seq(target_sequence), id=str(0), description=ref)
+        count = SeqIO.write(seq_obj, handle, "fasta")
+        for seq_i in range(len(full_df.index)):
+            print("seq_i:" + str(seq_i))
+            descr="CluSeq:" + str(round(full_df.iloc[seq_i,-3],5)) + "_sd:" + str(round(full_df.iloc[seq_i,-1],5))
+            print(descr)
+            seq_obj = SeqRecord(Seq(full_df.iloc[seq_i,0]), id=str(id_f), description=descr)
+            print(seq_obj)
+            count = SeqIO.write(seq_obj, handle, "fasta")
+            id_f+=1
+    print("Saved!")
 
 def calculate_perc_sd(full_df):
     full_df = full_df.fillna(value=0)
@@ -342,8 +357,161 @@ def aligner(full_df, target_sequence, align_method, filename, output_path, gop=3
     #Generate a visual alignment file using mview
     mview_file=output_path + "/" + filename.split("/")[-1].split(".")[-2] + ".html"
     mview_command='/media/data/AtteR/Attes_bin/mview -in fasta -html head -css on -coloring any ' + filename + '>' + mview_file
-    call([mview_command], shell=True)
-
+    #call([mview_command], shell=True)
+    #os.system('/media/data/AtteR/Attes_bin/mview -in fasta -html head -css on -coloring any {} > {}'.format(str(filename), str(mview_file))) 
     #subprocess.run(['/media/data/AtteR/Attes_bin/mview', '-in fasta', '-html head', '-css on', '-coloring any', filename, '>', mview_file])
 
 
+def translate_nt_aa(result, corr_frame):
+    out_of_frames=[0,1,2]
+    out_of_frames.remove(corr_frame)
+    refs_aa_frames={}
+    aa_and_perc={}
+
+    len(aa_and_perc)
+    for record in SeqIO.parse(result, "fasta"):
+        if record.id=="0":
+            refs_aa_frames["Frame:" + str(corr_frame)]=str(Seq(record.seq[corr_frame:]).translate())
+            for alt_frame in out_of_frames:
+                refs_aa_frames["Frame:" + str(alt_frame)]=str(Seq(record.seq[alt_frame:]).translate())
+        else:
+            aa_and_perc[">"+str(record.description) + "_transl.frame:" + str(corr_frame)]=str(Seq(record.seq[corr_frame:]).translate())
+
+    #you go over the ref seqs in different frames and align all the amplicons to them. save the alignment into the df's specific column. initially
+    #save into a list or such
+    ref_x_alignment={}
+    for frame_ref in refs_aa_frames.keys():
+        alignments_per_ref=[]
+        for ampl in aa_and_perc.keys():
+            matches=SequenceMatcher(None,refs_aa_frames[frame_ref],aa_and_perc[ampl])
+            seqs=[]
+            #you use range_line so that when you fill the remnants from left side of the match, you wont keep adding from
+            #beginning since in the end, we merge the seq list elements into the whole alignment of the amplicon against the ref
+            range_line=0
+            for i in range(len(matches.get_matching_blocks())):
+                match=matches.get_matching_blocks()[i]
+                seqs.append(len(refs_aa_frames[frame_ref][range_line:match.a])*"-"+str(aa_and_perc[ampl])[match.b:match.b+match.size])
+                range_line=match.a+match.size
+            alignments_per_ref.append(''.join(seqs))
+        ref_x_alignment[frame_ref + "|Ref:" +refs_aa_frames[frame_ref]]=alignments_per_ref
+    seq_info={"Seq_info:":aa_and_perc.keys()}
+    keys=list(aa_and_perc.keys())
+
+
+    seq_info=["Seq_info"]+list(aa_and_perc.keys())
+    ref_x_alig_list=[]
+    for keys, values in ref_x_alignment.items():
+        #print("".join(list((keys))[:]))
+        #ref_x_alig_list.append([("".join(list((keys))))]+list(values))
+        ref_x_alig_list.append([keys]+list(values))
+
+    df = pd.DataFrame(data= {seq_info[0]: seq_info[1:], ref_x_alig_list[0][0]:ref_x_alig_list[0][1:], ref_x_alig_list[1][0]:ref_x_alig_list[1][1:], ref_x_alig_list[2][0]:ref_x_alig_list[2][1:]})
+    return(df)
+#to show alignments to ref seq that has been translated in frame
+
+def find_colour(aa,color_scheme):
+    col=[key for key,val in color_scheme.items() if any(aa in s for s in val)]
+    out="".join(col)
+    if out==None or not col:
+        out="BLACK"
+        return(out)
+    else:
+        return(out)
+
+def aa_coloring(seq_infos, aligs1, aligs2, frame_info, output):
+    #using lesk color code scheme:
+    #color_scheme={"RED": [["#FF0000"], ["D", "E"]], "BLUE":[["#6495ED"],["K", "R"]], "GREEN":[["#9FE2BF"], ["C", "V", "I", "L", "P", "F", "Y", "M", "W"]], "ORANGE":[["#FF7F50"],["G", "A", "S", "T"]], "MAGENTA":[["#DE3163"], ["N", "Q", "H"]], "BLACK": [["#000000"],["-","|", "*"]]}
+    color_scheme={"RED": [["#FF0000"], ["D", "E"]], "BLUE":[["#6495ED"],["K", "R"]], "GREEN":[["#9FE2BF"], ["C", "V", "I", "L", "P", "F", "Y", "M", "W"]], "ORANGE":[["#FF7F50"],["G", "A", "S", "T"]], "MAGENTA":[["#DE3163"], ["N", "Q", "H"]], "BLACK": [["#000000"]]}
+
+    f = open(output,"w")
+    for info, a1,a2, frame in zip(seq_infos, aligs1, aligs2,frame_info):
+        aa1_list=[]
+        aa2_list=[]
+
+        print(frame)
+        a1,a2=a1[0:round(len(a1)*0.6)], a2[0:round(len(a2)*0.6)]
+        #f.write("<html> \n <body> <p>"+ "_".join(info.split("_")[:1]) +"</p> <p>")
+        f.write('<!DOCTYPE html><html> <head><link rel="stylesheet" href="format.css"></head> <meta charset="utf-8"> <body> <p>'+ "_".join(info.split("_")[:2]) + "<br>" + frame +'</p> <p>')
+
+    #go over the individual AAs of the alignemnts
+        #write the html colouring into a string var for each aa and append into a list, then join and make a massive string,
+        # then after all AAs have been iterated over for the certain seq, then write into the html 
+        for aa1,aa2, in zip(a1,a2):
+            try:
+                aa2_list.append('<span style="color:'+ color_scheme[find_colour(aa2,color_scheme)][0][0]+ '">' + aa2 + '</span>')
+            except KeyError:
+                print("Keyerror with aa2 being: " + aa2)
+            try:
+                aa1_list.append('<span style="color:'+ color_scheme[find_colour(aa1,color_scheme)][0][0]+ '">' + aa1 + '</span>')
+            except KeyError:
+                print("Keyerror with aa1 being: " + aa1)
+            
+            # aa2_list.append('<span style="color:'+ color_scheme[find_colour(aa2,color_scheme)][0][0]+ '">' + aa2 + '</span>')
+            # aa1_list.append('<span style="color:'+ color_scheme[find_colour(aa1,color_scheme)][0][0]+ '">' + aa1 + '</span>')
+
+            #print("============")
+        coloured_ref="".join(aa2_list)
+
+        coloured_seq="".join(aa1_list)
+
+        f.write(coloured_ref +"<br>")
+        f.write(coloured_seq)
+        f.write("</p>")
+
+    f.write("</body></html>")
+    f.close()
+out="/media/data/AtteR/projects/hiti/AA_3p_mcherry_coloured_alignment.html"
+
+
+#now you have a df where first col contains the information about the specific amplicon, the cols 1-3 are
+#named based on the ref codon frame + the entire translated AA seq
+
+#Go over each alignment col by row, find the longest match and align these next to each other.
+#then move onto the next col (ref seq translated using a different frame), find the longest consequtive seq
+#in the amplicon and align this to the ref too. repeat with the last col as well.
+
+#when aligning to the ref using e.g. ref1 (in frame) and amplicon 1, cut off the amplicon 1 after
+#the longest conseq. seq has been found. Then take the same amplicon but when it has been aligned to the
+#ref2 (out of frame), find the longest seq, remove the extra "---" from the beginning relative to the 
+#amplicon 1 and then align to the ref. merge the two. 
+
+def visualise_aa_hybrid_alignments(df, output_html): #output as html
+    seqinfos=[]
+    aligs_merged_seq=[]
+    aligs_merged_ref=[]
+    frame_info=[]
+    for ampl_row in range(round(len(df.index)*0.1)):
+        match1 = SequenceMatcher(None, df.columns[1].split(":")[-1], df.iloc[ampl_row,1]).find_longest_match(0, len(df.columns[1].split(":")[-1]), 0, len(df.iloc[ampl_row,1]))
+        end_of_match=match1.b+match1.size
+        matched_ampl1= len(df.columns[1].split(":")[-1][:match1.a])*"-" + str(df.iloc[ampl_row,1][match1.b:]) + "|"
+        seq_inframe1=matched_ampl1[0:end_of_match] + "|"
+        ref_inframe1=df.columns[1].split(":")[-1][0:end_of_match] + "|"
+        #get the matches with the other reading frames (i.e. the last 2 cols of the df)
+        match2_a = SequenceMatcher(None, df.columns[2].split(":")[-1], df.iloc[ampl_row,2]).find_longest_match(0, len(df.columns[2].split(":")[-1]), 0, len(df.iloc[ampl_row,2]))
+        match2_b = SequenceMatcher(None, df.columns[3].split(":")[-1], df.iloc[ampl_row,3]).find_longest_match(0, len(df.columns[3].split(":")[-1]), 0, len(df.iloc[ampl_row,3]))
+        if match2_a.size>match2_b.size:
+            matched_ampl2= len(df.columns[2].split(":")[-1][:match2_a.a])*"-" + str(df.iloc[ampl_row,2][match2_a.b:]) + "|"
+            ref_outframe2=df.columns[2].split(":")[-1][0:end_of_match] + "|"
+            # seq_inframe2=matched_ampl2[0:end_of_match]
+            seq_outframe=matched_ampl2[end_of_match:]
+            frame_info.append("Frame:+1 " + ref_inframe1.index("|")*" " + "Frame:0")
+        else:
+            matched_ampl2= len(df.columns[3].split(":")[-1][:match2_b.a])*"-" + str(df.iloc[ampl_row,3][match2_b.b:]) + "|"
+            ref_outframe2=df.columns[3].split(":")[-1][end_of_match:] + "|"
+            seq_outframe=matched_ampl2[end_of_match:]
+            # seq_inframe2=matched_ampl2[0:end_of_match]
+            seq_outframe=matched_ampl2[end_of_match:]
+            frame_info.append("Frame:+1 " + ref_inframe1.index("|")*" " + "Frame:+2")
+            print("Frame:+1 " + ref_inframe1.index("|")*" " + "Frame:+2")
+        merged_align_seq=seq_inframe1+seq_outframe
+
+        merged_align_ref=ref_inframe1+ref_outframe2
+        header_info=df.iloc[ampl_row,0]
+        #add one more line which adds the frames
+
+        seqinfos.append(df.iloc[ampl_row,0])
+        #aligs_merged_seq.append(merged_align_seq[0:round(len(aligs_merged_seq)*0.6)])
+        aligs_merged_seq.append(merged_align_seq)
+        #aligs_merged_ref.append(merged_align_ref[0:round(len(merged_align_ref)*0.6)])
+        aligs_merged_ref.append(merged_align_ref)
+    aa_coloring(seqinfos,aligs_merged_seq,aligs_merged_ref, frame_info, output_html)
