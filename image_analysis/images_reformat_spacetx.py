211b38371b80e7165670efce3fc40edff98eee80
atte11 <70641619+Atte-Oskari-Rasanen@users.noreply.github.com>
2022-07-11 17:30:22 +0200

    Update images_reformat_spacetx.py
    a functional set of scripts for organising image data into spacetx format required by starfish.

--- image_analysis/images_reformat_spacetx.py
+++ image_analysis/images_reformat_spacetx.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import os
 from PIL import Image, ImageOps
@@ -36,6 +35,17 @@ def fovs_gen(im_arrs, tile_size, tile_points, base_path, manifest_file, image_ty
             except FileExistsError:
                 print("Directory already exists")
             base_path=nucl_path
+            FOV_dir= {"default_tile_format": "TIFF", "dimensions": ["z","xc","x","yc","y","zc","c","r"],"extras": {},
+            "shape": {
+                "c": 1,
+                "r": 0,
+                "z": 0
+            },
+            "tiles": [],
+            "version": "0.1.0"
+            }
+
+
     else:
         prim_dir="primary"
         prim_path = os.path.join(base_path, prim_dir)
@@ -44,26 +54,32 @@ def fovs_gen(im_arrs, tile_size, tile_points, base_path, manifest_file, image_ty
         except FileExistsError:
             print("Directory already exists")
         base_path=prim_path
+        FOV_dir= {"default_tile_format": "TIFF", "dimensions": ["z","xc","x","yc","y","zc","c","r"],"extras": {},
+            "shape": {
+                "c": 5,
+                "r": 0,
+                "z": 0
+            },
+            "tiles": [],
+            "version": "0.1.0"
+            }
+
     #coord_fovs={'zc_min':0.005, 'zc_max':0.010}
+    FOV_dir_orig=FOV_dir.copy()
     Y_points=tile_points[0]
     X_points=tile_points[1]
     fov_i=0
-    FOV_dir= {"default_tile_format": "TIFF", "dimensions": ["z","xc","x","yc","y","zc","c","r"],"extras": {},
-                "shape": {
-                    "c": 5,
-                    "r": 0,
-                    "z": 0
-                },
-                "tiles": [],
-                "version": "0.1.0"
-                }
-
     for y_i, y in enumerate(tile_points[0][:-2]):
         for x_i, x in enumerate(tile_points[1][:-2]):
 
             #coordinate_tiles=[]
             #save the tile from each im to the fov dir
             if image_type=='nuclei':
+                split = im_arrs[0][y:y+tile_size, x:x+tile_size]                
+                tile_im=Image.fromarray(split)
+                tilename=f'{image_type}-f{fov_i}-r0-c0-z0.tiff'
+                tile_im.save(f'{base_path}/{tilename}')
+
                 xmin=round(x/tile_points[1][-1], 6)
                 xmax=round(X_points[x_i+1]/im_arrs[0].shape[0],6)
                 ymin=round(y/tile_points[0][-1])
@@ -73,18 +89,19 @@ def fovs_gen(im_arrs, tile_size, tile_points, base_path, manifest_file, image_ty
                 tilename=f'{image_type}-f{fov_i}-r0-c0-z0.tiff'
                 fov_i_dir={'coordinates': {'xc': [xmin, xmax], 'yc': [ymin, ymax],
                 'zc': [zmin, zmax]}, 'file': tilename,
-                'indices': {'c': c, 'r': 0, 'z': 0}, 'tile_format': 'TIFF',
+                'indices': {'c': 0, 'r': 0, 'z': 0}, 'tile_format': 'TIFF',
                 'tile_shape': {'x': tile_size, 'y': tile_size}}
                 FOV_dir["tiles"].append(fov_i_dir)
+                fov_key=f'fov_{fov_i}'
+                im_name=f'{base_path}/{image_type}-images-fov_{fov_i}.json'
+                current_fov_dir={fov_key: im_name}
+                manifest_file["contents"].update(current_fov_dir)
 
-                #split = im[y:y+tile_size, x:x+tile_size]
-                #print(split)
-                #tile_im=Image.fromarray(split)
-                #tile_im.save(f'{base_path}/{tilename}')
-                #img_tiles.append(split)
+                with open(f'{base_path}/{image_type}-images-fov_{fov_i}.json', "w") as jsonfile:
+                    json.dump(FOV_dir,jsonfile)
 
             else:
-                for c,im in enumerate(im_arrs):
+                for c,im in enumerate(im_arrs[1:]):
                     split = im[y:y+tile_size, x:x+tile_size]                
                     tile_im=Image.fromarray(split)
                     tilename=f'{image_type}-f{fov_i}-r0-c{c}-z0.tiff'
@@ -106,18 +123,14 @@ def fovs_gen(im_arrs, tile_size, tile_points, base_path, manifest_file, image_ty
                 with open(f'{base_path}/{image_type}-images-fov_{fov_i}.json', "w") as jsonfile:
                     json.dump(FOV_dir,jsonfile)
 
-                FOV_dir= {"default_tile_format": "TIFF", "dimensions": ["z","xc","x","yc","y","zc","c","r"],"extras": {},
-                "shape": {
-                    "c": 5,
-                    "r": 0,
-                    "z": 0
-                },"tiles": [],"version": "0.1.0"}
                 FOV_dir["tiles"]=[]
                 fov_key=f'fov_{fov_i}'
                 im_name=f'{base_path}/{image_type}-images-fov_{fov_i}.json'
                 current_fov_dir={fov_key: im_name}
                 manifest_file["contents"].update(current_fov_dir)
-                fov_i+=1
+            FOV_dir=FOV_dir_orig
+            fov_i+=1
+
     with open(f'{base_path}/{image_type}_images.json', 'w') as jsonfile:
         json.dump(manifest_file,jsonfile) 
     print(f'manifest file saved with all the fovs as: {base_path}/{image_type}_images.json')
@@ -136,17 +149,14 @@ def tiles_gen(im_paths, tile_size,overlap, base_path,exp_file, codebook, manifes
         #img_padded = np.pad(img, ((x_pad,x_pad), (y_pad,y_pad)), constant_values=0.0, mode="constant")
         im_arrs.append(img)
         print(f'Image imported, shape is {img.shape}')
-        #im_arrs[path.split("/")[8]]=img_padded
-    print(f'im_arr length: {len(im_arrs)}')
-    print(f'im_arr 1: {im_arrs[0].shape}')
 
     X_points = start_points(im_arrs[0].shape[0], tile_size, overlap)
     Y_points = start_points(im_arrs[0].shape[1], tile_size, overlap)
     tile_points=[Y_points, X_points]
 
-    #nuclei_arr=fovs_gen(im_arrs, tile_size, tile_points, base_path,nucl_manifest,"nuclei")
+    nuclei_arr=fovs_gen(im_arrs, tile_size, tile_points, base_path,manifest_file,"nuclei")
     #nuclei_arr=0
-    prim_arr=fovs_gen(im_arrs, tile_size, tile_points, base_path,manifest_file,"primary")
+    prim_arr=fovs_gen(im_arrs[1:], tile_size, tile_points, base_path,manifest_file,"primary")
     #prim_arr=0
 
     #pass the manifest files into the functions and update as you go through the loops
